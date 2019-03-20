70c9389c28a1035cffef3f1295921fd4d372bc98
cormic2000 <43865040+cormic2000@users.noreply.github.com>
2019-03-20 17:22:24 +0000

    Add files via upload

---
+++ PrintBoard.h
@@ -0,0 +1,16 @@
+int PrintBoard(void)
+{
+  int count, counts=1, a=1;
+  printf("    1    2    3    4    5    6    7    8    9 \n");
+   for(count = 1;count!=7;count++)
+   {
+    printf("%d ", a);
+    for(counts = 1;counts!=10;counts++)
+    {
+     printf("| %c |",array[count][counts]);
+    }
+    printf("\n");
+    a++;
+    counts = 1;
+  }
+}
