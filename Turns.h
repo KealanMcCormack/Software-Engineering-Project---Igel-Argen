f52c8643e11390cc1be727b8cd430d56034874c2
cormic2000 <43865040+cormic2000@users.noreply.github.com>
2019-04-22 17:40:10 +0100

    Add files via upload

--- Turns.h
+++ Turns.h
@@ -5,7 +5,7 @@ void Turns(int dice)
   char hold;
   printf("\nTime to Move\n");
   dice = dice - 1;
-    printf("    1    2    3    4    5    6    7    8    9 \n");
+    printf("  1    2    3    4    5    6    7    8    9 \n");
   for(count=0;count<9;count++)
   {
    printf("| %c |", boardIndex[dice][count][0]);
